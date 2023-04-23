c790bee771193e8efb7d729da29abd20962b8d1d
masoud8840 <masoudgharedaghi@yahoo.com>
2023-04-23 18:54:48 +0330

    added TheHeader cmp

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-
+  <the-header></the-header>
 </template>
+
+<script setup>
+import TheHeader from "./components/layout/TheHeader.vue";
+</script>
---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav class="navbar container">
+      <h1 class="brand text-brand">TEAM Manager</h1>
+      <ul class="navbar-inner">
+        <li>
+          <a href="#">To Do</a>
+        </li>
+        <li>
+          <a href="#">Example</a>
+        </li>
+        <li>
+          <a href="#">Example</a>
+        </li>
+        <li>
+          <a href="#">Example</a>
+        </li>
+      </ul>
+      <section class="user-profile">
+        <button>
+          <h5>Julie Jones</h5>
+          <img src="/ArrowDown_Light.svg" alt="arrow-down" />
+        </button>
+        <img
+          src="/UserProfile.svg"
+          alt="user-profile"
+          class="user-profile-img"
+        />
+      </section>
+    </nav>
+  </header>
+</template>
