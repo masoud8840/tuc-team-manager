15078e0660fd30687a34d6f9b89c7be3b6dae988
masoud8840 <masoudgharedaghi@yahoo.com>
2023-04-25 13:41:11 +0330

    almost done modal cmp

---
+++ src/components/UI/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+  <dialog open class="modal">
+    <section class="modal-inner">
+      <div class="modal-header">
+        <h4>{{ props.modalTitle }}</h4>
+        <button>
+          <Plus />
+        </button>
+      </div>
+      <div class="modal-body">
+        <slot></slot>
+      </div>
+    </section>
+  </dialog>
+</template>
+
+<script setup>
+import Plus from "../icon/Plus.vue";
+const props = defineProps(["modalTitle"]);
+</script>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,9 +9,33 @@
       <add-task></add-task>
     </article>
   </section>
+
+  <teleport to="body">
+    <Modal modal-title="Add New To Do">
+      <div class="input-group row">
+        <label for="taskTitle">Task title</label>
+        <input type="text" id="taskTitle" placeholder="What you wanna To Do?" />
+      </div>
+      <div class="input-group row">
+        <div class="input-group column">
+          <!-- TODO: <Checkbox /> -->
+        </div>
+        <label>Team</label>
+      </div>
+      <div class="input-group row">
+        <label for="assignedTo">Assigned to</label>
+        <select id="assignedTo" disabled>
+          <option value="masoud">Masoud</option>
+          <option value="abolfazl">abolfazl</option>
+          <option value="maedeh">maedeh</option>
+        </select>
+      </div>
+    </Modal>
+  </teleport>
 </template>
 
 <script setup>
 import TaskItem from "../components/UI/TaskItem.vue";
 import AddTask from "../components/UI/AddTask.vue";
+import Modal from "../components/UI/Modal.vue";
 </script>
